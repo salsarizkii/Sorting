51317630c0b80a7f093f4a61cf3806b333554547
salsarizki <salsarizkisaputrii@gmail.com>
2023-03-01 23:06:04 +0700

    fix "pilihan"

--- try1.cpp
+++ try1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int pilihan();
+
 using namespace std;
 
 int maks = 0;
@@ -14,6 +16,7 @@ int arrayList[3] = { a, b, c};
 int main(void){
     
     int pilUrutan;
+    // puts("\n");
     puts(" [1] Angka terbesar");
     puts(" [2] Angka terkecil");
     printf("pilihanmu -> ");
@@ -25,7 +28,8 @@ int main(void){
                 maks = arrayList[i];
             }
         }
-        printf("%d", maks);
+        printf("%d\n\n", maks);
+        pilihan();
     } else if(pilUrutan == 2) {
         minimum = arrayList[0];
         for (int j = 1; j < 3; j++) {
@@ -33,9 +37,28 @@ int main(void){
                 minimum = arrayList[j];
             }
         }
-        printf("%d", minimum);
+        printf("%d\n\n", minimum);
+        pilihan();
+    } else if(pilUrutan != 1 || 2){
+        puts("\npilihan salah! pilih ulang ");
+        main();
     }
-
-    
     return 0;
+}
+
+int pilihan(){
+    puts(" [1] pilih ulang");
+    puts(" [2] keluar program");
+    int pilih;
+    printf("pilihanmu -> ");
+    scanf("%d", &pilih);
+    puts("\n");
+    if(pilih == 1){
+        main();
+    } else if(pilih == 2){
+        exit(0);
+    } else {
+        puts("\npilihan salah! pilih ulang ");
+        pilihan();
+    }
 }
